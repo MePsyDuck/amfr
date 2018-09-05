55fb74a7a54276ce2e90829dacef27d7dd6a9500
MePsyDuck <sumit231996@gmail.com>
2018-09-05 18:34:10 +0530

    Refactoring, Organising imports

--- facial_recog/app/training_util.py
+++ facial_recog/app/training_util.py
@@ -2,9 +2,9 @@ import os
 
 import cv2
 
-from facial_recog.app.classifier_util import get_class_subjects
-from facial_recog.app.config import cascade_dir, default_method
-from facial_recog.app.face_detection import detect_face
+from .config import cascade_dir, detect_method
+from .face_detection import detect_face
+from .util import get_class_subjects
 
 
 def prepare_training_data(class_id):
@@ -13,27 +13,23 @@ def prepare_training_data(class_id):
     subjects = get_class_subjects(class_id)
 
     for subject in subjects:
-        subject_dir_path = cascade_dir[default_method] + os.pathsep + subject
+        subject_dir_path = cascade_dir[detect_method] + os.pathsep + subject
         subject_images = os.listdir(subject_dir_path)
 
-        for image_name in subject_images:
-            if image_name.startswith("."):
-                continue
+        if subject_images in None:
+            print("No images found for subject: " + subject)
+        else:
+            for image_name in subject_images:
+                if image_name.startswith("."):
+                    continue
 
-            image_path = subject_dir_path + "/" + image_name
-            image = cv2.imread(image_path)
+                image_path = subject_dir_path + "/" + image_name
+                image = cv2.imread(image_path)
 
-            cv2.imshow("Training on image...", image)
-            cv2.waitKey(100)
+                face, rect = detect_face(img=image, class_id=class_id)
 
-            face, rect = detect_face(img=image, class_id=class_id)
-
-            if face is not None:
-                faces.append(face)
-                labels.append(subject)
-
-    cv2.destroyAllWindows()
-    cv2.waitKey(1)
-    cv2.destroyAllWindows()
+                if face is not None:
+                    faces.append(face)
+                    labels.append(subject)
 
     return faces, subjects
