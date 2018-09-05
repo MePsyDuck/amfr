f27e96ec38a2c4173f9c80d597387f2fb25e3c96
MePsyDuck <sumit231996@gmail.com>
2018-09-05 18:32:08 +0530

    Removed unused code
    Signed-off-by: MePsyDuck <sumit231996@gmail.com>

--- facial_recog/app/face_detection.py
+++ facial_recog/app/face_detection.py
@@ -1,16 +1,11 @@
-import os
-
 import cv2
 
-from facial_recog.app.config import cascade_dir, default_method
+from .config import cascade_dir, detect_method
 
 
 def detect_face(img, class_id):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    classifier_loc = cascade_dir[default_method] + class_id + '.xml'
-
-    if not os.path.exists(classifier_loc):
-        print("Classifier not found for " + class_id)
+    classifier_loc = cascade_dir[detect_method] + class_id + '.xml'
 
     face_cascade = cv2.CascadeClassifier(classifier_loc)
     faces = face_cascade.detectMultiScale(image=gray, scaleFactor=1.2, minNeighbors=5)
