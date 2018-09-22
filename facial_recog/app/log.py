917c688505f6b093517f5c57939ceb513fdfe1ca
MePsyDuck <sumit231996@gmail.com>
2018-09-22 18:47:07 +0530

    Fix for no log directory created

--- facial_recog/app/log.py
+++ facial_recog/app/log.py
@@ -6,6 +6,7 @@ from .config import log_dir
 
 
 def setup_logger():
+    os.mkdir(log_dir)
     logging.basicConfig(
         filename=os.path.join(log_dir, str(datetime.datetime.now().strftime('%Y%m%d_%H%M%S.log'))),
         filemode='w',
