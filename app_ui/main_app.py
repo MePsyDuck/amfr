bd856f8da36953506db1ec76b43f0ba55fe870c2
MePsyDuck <sumit231996@gmail.com>
2018-09-25 20:35:12 +0530

    Formatting fixes

--- app_ui/main_app.py
+++ app_ui/main_app.py
@@ -39,11 +39,9 @@ root.title("Attendance Monitoring")
 
 Label(root, text="Student ID").grid(row=0)
 Label(root, text="Class ID").grid(row=1)
-Label(root, text="Class ID").grid(row=1)
 
 e1 = Entry(root)
 e2 = Entry(root)
-
 e1.grid(row=0, column=1)
 e2.grid(row=1, column=1)
 
